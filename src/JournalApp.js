42e6d1579f10a024865b5abfa5b2f38fe8012c0c
Hazael <ingwalterlopez134@gmail.com>
2021-12-22 12:10:56 -0600

    Inicio del proyecto - JournalApp

---
+++ src/JournalApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const JournalApp = () => {
+    return (
+        <React.Fragment>
+            <h1>Journal App</h1>
+            <hr/>
+        </React.Fragment>
+    )
+}
+
+export default JournalApp
